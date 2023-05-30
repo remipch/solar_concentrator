37fc5c596c80b92a4c525140f1b37b8b2847bf5d
remipch <75692116+remipch@users.noreply.github.com>
2023-05-30 19:45:19 +0200

    motors_controller: update commands after mech changed

--- sun_follower_panel/motors_controller/send_commands.py
+++ sun_follower_panel/motors_controller/send_commands.py
@@ -26,32 +26,38 @@ while True:
         exit()
     elif ch == 's':
         writeCommand("Status", "s")
-    elif ch == 'r':
-        writeCommand("Release all", "o:21,1000")
-    elif ch == 't':
-        writeCommand("Tense all", "o:42,1000,80")
+    elif ch == 'a':
+        writeCommand("Unroll all", "o:21,1000,80")
+    elif ch == 'A':
+        writeCommand("Roll all", "o:42,1000,80")
+    elif ch == 'u':
+        writeCommand("Unroll up", "o:1,1000,30")
+    elif ch == 'U':
+        writeCommand("Roll up", "o:2,5000,30")
+    elif ch == 'j':
+        writeCommand("Unroll down right", "o:4,1000,30")
+    elif ch == 'J':
+        writeCommand("Roll down right", "o:8,5000,30")
+    elif ch == 'h':
+        writeCommand("Unroll down left", "o:16,1000,30")
+    elif ch == 'H':
+        writeCommand("Roll down left", "o:32,5000,30")
     elif ch == '8':
-        writeCommand("UP", "o:20,500;o:2,5000,30")
+        writeCommand("Step UP", "o:20,500;o:2,2000,50")
     elif ch == '9':
-        #writeCommand("UP_RIGHT", "o:16,1000;o:2,5000,30")
-        writeCommand("UP_RIGHT", "o:16,500,30;o:2,1500,30")
+        writeCommand("Step UP_RIGHT", "o:16,500,30;o:2,1500,30")
     elif ch == '6':
-        #writeCommand("RIGHT", "o:16,200;o:8,5000,25")
-        writeCommand("RIGHT", "o:16,500,30;o:8,1500,30")
+        writeCommand("Step RIGHT", "o:16,500,30;o:8,1500,30")
     elif ch == '3':
-        #writeCommand("DOWN_RIGHT", "o:17,500;o:16,500;o:42,5000,80")
-        writeCommand("DOWN_RIGHT", "o:1,500,30;o:8,1500,30")
+        writeCommand("Step DOWN_RIGHT", "o:1,500,30;o:8,1500,20")
     elif ch == '2':
-        writeCommand("DOWN", "o:1,200;o:40,5000,50")
+        writeCommand("Step DOWN", "o:1,200;o:40,5000,50")
     elif ch == '1':
-        #writeCommand("DOWN_LEFT", "o:5,500;o:42,5000,80")
-        writeCommand("DOWN_LEFT", "o:1,200,30;o:32,1500,30")
+        writeCommand("Step DOWN_LEFT", "o:1,500,30;o:32,1500,20")
     elif ch == '4':
-        #writeCommand("LEFT", "o:4,200;o:32,5000,20")
-        writeCommand("LEFT", "o:4,500,30;o:32,1500,30")
+        writeCommand("Step LEFT", "o:4,500,30;o:32,1500,30")
     elif ch == '7':
-        #writeCommand("UP_LEFT", "o:4,1000;o:2,5000,25")
-        writeCommand("UP_LEFT", "o:4,500,30;o:2,1500,30")
+        writeCommand("Step UP_LEFT", "o:4,500,30;o:2,1500,30")
     else:
         print("unkown key")
         continue
