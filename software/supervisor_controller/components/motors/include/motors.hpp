ebc35f6de0531de003efb7314dcc18ae39c5c5ec
remipch <75692116+remipch@users.noreply.github.com>
2023-10-12 23:43:49 +0200

    motors: add blocking move function

--- software/supervisor_controller/components/motors/include/motors.hpp
+++ software/supervisor_controller/components/motors/include/motors.hpp
@@ -4,10 +4,16 @@
 
 motors_full_status_t motors_get_status();
 
+// Must be called once before any other functions
 void motors_init();
 
 void motors_start_move(motors_direction_t direction);
 
 void motors_start_move_one_step(motors_direction_t direction);
+
+// Move one step and wait motors to finish their move.
+// It's the only synchronous function, other functions push
+// an event and returns immediately
+void motors_blocking_move_one_step(motors_direction_t direction);
 
 void motors_stop();
--- software/supervisor_controller/components/motors/motors.cpp
+++ software/supervisor_controller/components/motors/motors.cpp
@@ -18,16 +18,20 @@ static const char* TAG = "motors";
 // These technical choices are not exposed publicly but only kept here internally
 static bool is_initialized = false;
 static esp_event_loop_handle_t event_loop;
-static ESP_EVENT_DEFINE_BASE(MOTORS_EVENTS); // (not declared in header because only used here internally)
-static TickType_t EVENT_LOOP_TIMEOUT_MS = 100;
-static const uint64_t TIMER_PERIOD_MS = 100;
+static ESP_EVENT_DEFINE_BASE(MOTORS_EVENTS);
+static const int EVENT_LOOP_TIMEOUT_MS = 5000;
+static const long unsigned int TIMER_PERIOD_MS = 200;
 static esp_timer_handle_t periodic_timer;
+static const int STATUS_MUTEX_TIMEOUT_MS = 100;
 static SemaphoreHandle_t status_mutex;
+static const int BLOCKING_MOVE_SEMAPHORE_TIMEOUT_MS = 5000;
+static SemaphoreHandle_t blocking_move_semaphore; // used as synchronization when a blocking move is finished
 
 enum {
     MOTORS_INIT_EVENT,
     MOTORS_START_MOVE_EVENT,
     MOTORS_START_MOVE_ONE_STEP_EVENT,
+    MOTORS_BLOCKING_MOVE_ONE_STEP_EVENT,
     MOTORS_STOP_EVENT,
     MOTORS_PERIODIC_UPDATE_EVENT,
 };
@@ -47,17 +51,16 @@ motors_full_status_t motors_get_status()
 {
     // 'status' is only writen by the event_loop task, it can be read by any other task.
     // 'status_mutex' ensures 'status' variable consistency
-    // portMAX_DELAY is used but the code inside the mutex-protected bloc is short
-    // and should not run for a long time.
-    xSemaphoreTake(status_mutex, portMAX_DELAY);
+    assert(xSemaphoreTake(status_mutex, pdMS_TO_TICKS(STATUS_MUTEX_TIMEOUT_MS)));
     auto status = current_status;
     xSemaphoreGive(status_mutex);
     return status;
 }
 
+bool must_signal_end_of_move = false;
+
 static void motors_event_handler(void* handler_args, esp_event_base_t base, int32_t id, void* event_data)
 {
-    int64_t time_since_boot_ms = esp_timer_get_time() / 1000;
     switch (id) {
     case MOTORS_INIT_EVENT:
         motors_logic_init();
@@ -72,20 +75,34 @@ static void motors_event_handler(void* handler_args, esp_event_base_t base, int3
         motors_logic_start_move_one_step(direction);
         break;
     }
+    case MOTORS_BLOCKING_MOVE_ONE_STEP_EVENT: {
+        motors_direction_t direction = *((motors_direction_t*)event_data);
+        motors_logic_start_move_one_step(direction);
+        must_signal_end_of_move = true;
+        break;
+    }
     case MOTORS_STOP_EVENT:
         motors_logic_stop();
         break;
     case MOTORS_PERIODIC_UPDATE_EVENT: {
-        motors_logic_periodic_update(time_since_boot_ms);
+        // Restart timer before running periodic update function
+        ESP_ERROR_CHECK(esp_timer_start_once(periodic_timer, TIMER_PERIOD_MS * 1000));
+        motors_logic_periodic_update(esp_timer_get_time() / 1000);
         break;
     }
     default:
-        ESP_LOGE(TAG, "Unkown event : ignored");
+        // Unkown event
+        assert(false);
     }
 
-    xSemaphoreTake(status_mutex, portMAX_DELAY);
+    assert(xSemaphoreTake(status_mutex, pdMS_TO_TICKS(STATUS_MUTEX_TIMEOUT_MS)));
     current_status = motors_logic_get_status();
     xSemaphoreGive(status_mutex);
+
+    if(must_signal_end_of_move && current_status.state==motors_state_t::STOPPED) {
+        must_signal_end_of_move = false;
+        xSemaphoreGive(blocking_move_semaphore);
+    }
 }
 
 void post_event(int32_t event_id, void* data = NULL, size_t data_size = 0)
@@ -134,9 +151,16 @@ void motors_init()
             .skip_unhandled_events = true,
         };
         ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
-        ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, TIMER_PERIOD_MS * 1000));
+
+        // Don't start a periodic timer here but a one shot timer which will be
+        // restarted at every periodic update.
+        // Using a periodic timer would saturate the event loop with MOTORS_PERIODIC_UPDATE_EVENT
+        // in case of some events taking a long time to execute, making all future events delayed after them.
+        ESP_ERROR_CHECK(esp_timer_start_once(periodic_timer, TIMER_PERIOD_MS * 1000));
 
         status_mutex = xSemaphoreCreateMutex();
+
+        blocking_move_semaphore = xSemaphoreCreateBinary();
 
         is_initialized = true;
     }
@@ -152,6 +176,13 @@ void motors_start_move(motors_direction_t direction)
 void motors_start_move_one_step(motors_direction_t direction)
 {
     post_event(MOTORS_START_MOVE_ONE_STEP_EVENT, &direction, sizeof(motors_direction_t));
+}
+
+void motors_blocking_move_one_step(motors_direction_t direction)
+{
+    assert(!xSemaphoreTake(blocking_move_semaphore, 0));
+    post_event(MOTORS_BLOCKING_MOVE_ONE_STEP_EVENT, &direction, sizeof(motors_direction_t));
+    assert(xSemaphoreTake(blocking_move_semaphore, pdMS_TO_TICKS(BLOCKING_MOVE_SEMAPHORE_TIMEOUT_MS)));
 }
 
 void motors_stop()
