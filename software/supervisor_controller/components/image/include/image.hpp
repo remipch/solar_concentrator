1864160836a1b77c3a0bb3ce9eb2c261597fd310
remipch <75692116+remipch@users.noreply.github.com>
2023-10-18 21:27:57 +0200

    image: add usefull rectangle type

--- software/supervisor_controller/components/image/include/image.hpp
+++ software/supervisor_controller/components/image/include/image.hpp
@@ -6,3 +6,12 @@
 #include "CImg.h"
 
 using namespace cimg_library;
+
+// Useful types not defined by CImg libs
+// but are very useful when dealing with images
+struct rectangle_t {
+    int left_px;
+    int top_px;
+    int right_px;
+    int bottom_px;
+};
