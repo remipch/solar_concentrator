ed459f24b6a5456a595936acb2df4f6592e87767
remipch <75692116+remipch@users.noreply.github.com>
2024-03-16 15:15:47 +0100

    web_log: increase buffer size

--- software/supervisor_controller/components/web_custom/web_log.hpp
+++ software/supervisor_controller/components/web_custom/web_log.hpp
@@ -3,7 +3,7 @@
 // Max size of log text cached between calls to 'web_log_get_last'
 // including terminating NULL char
 // If web_log_get_last is not called frequently enough, last logs will be lost
-const int LOG_BUFFER_SIZE = 500;
+const int LOG_BUFFER_SIZE = 1000;
 
 void web_log_init();
 
