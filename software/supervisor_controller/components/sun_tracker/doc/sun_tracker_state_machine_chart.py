142364c9b075ed69f7ebac1c25a5beb7f4e5d2c8
remipch <75692116+remipch@users.noreply.github.com>
2024-08-05 22:39:27 +0200

    motors: simplify state machine chart

--- software/supervisor_controller/components/sun_tracker/doc/sun_tracker_state_machine_chart.py
+++ software/supervisor_controller/components/sun_tracker/doc/sun_tracker_state_machine_chart.py
@@ -6,37 +6,38 @@
 
 import httpimport
 
-with httpimport.github_repo('remipch', 'svg_chart', ref='d334810f558912f6617943ed7270ff2490c5b5c7'):
+with httpimport.github_repo('remipch', 'svg_chart', ref='26eeccf6c8ebdba7bfa6ac5d9352b09bc792b5ac'):
   from svg_chart import *
 
 chart = Chart(node_width=200)
 
 uninitialized = Node(chart, 1, -1.5, "UNINITIALIZED", shape=NodeShape.ROUNDED_RECTANGLE, color="#bcd7ff")
 idle = Node(chart, 1, 0, "IDLE", shape=NodeShape.ROUNDED_RECTANGLE, color="#bcd7ff")
+idle_back = Point(chart, 2,0)
 start_cond_1 = Node(chart, 1, 2, shape=NodeShape.DIAMOND)
-result_error = Node(chart, 2, 2, "result = ERROR")
+result_error = Point(chart, 2.5, 2)
 start_cond_2 = Node(chart, 1, 4, shape=NodeShape.DIAMOND)
-result_success = Node(chart, 3, 4, "result = SUCCESS")
-start_move = Node(chart, 1, 6, "start_move(direction)")
-tracking = Node(chart, 1, 8, "TRACKING", shape=NodeShape.ROUNDED_RECTANGLE, color="#bcd7ff")
-stopping = Node(chart, 3, 8, "STOPPING", shape=NodeShape.ROUNDED_RECTANGLE, color="#bcd7ff")
+result_success = Point(chart, 3, 4)
+tracking = Node(chart, 1, 6, "TRACKING", shape=NodeShape.ROUNDED_RECTANGLE, color="#bcd7ff")
+tracking_back = Point(chart, 0.2, 6)
+stopping = Node(chart, 3.2, 6, "STOPPING", shape=NodeShape.ROUNDED_RECTANGLE, color="#bcd7ff")
 
-# Note: edges order creation is used to order multiple edges appearing on the same node border
 Edge(chart, uninitialized, idle, "->")
-Edge(chart, idle, start_cond_1, "->", "START")
-Edge(chart, start_cond_1, result_error, "-->", "else")
-Edge(chart, start_cond_1, start_cond_2, "->", "detection.result==SUCCESS")
-Edge(chart, start_cond_2, result_success, "->", "detection.direction==NONE")
-Edge(chart, start_cond_2, start_move, "-->", "else")
-Edge(chart, start_move, tracking, "->")
+Edge(chart, idle, start_cond_1, "->", "START / sun_tracker_logic_detect()")
+Edge(chart, idle_back, idle, "->")
+Edge(chart, start_cond_1, result_error, "-", "[else] / result=ERROR")
+Edge(chart, start_cond_1, start_cond_2, "->", "[detection.result==SUCCESS]")
+Edge(chart, start_cond_2, result_success, "-", "[detection.direction==NONE] / result=SUCCESS")
+Edge(chart, start_cond_2, tracking, "->", "[else] / motors_start_move(direction)")
 Edge(chart, tracking, stopping, "->", "STOP")
-Edge(chart, stopping, idle, "->", "MOTORS_STOPPED", layout=EdgeLayout.RIGHT_RIGHT_CURVED)
-Edge(chart, result_success, idle, "->", layout=EdgeLayout.RIGHT_RIGHT_CURVED)
-Edge(chart, result_error, idle, "->", layout=EdgeLayout.RIGHT_RIGHT_CURVED)
-Edge(chart, tracking, start_cond_1, "->", "MOTORS_STOPPED", layout=EdgeLayout.LEFT_LEFT_CURVED)
+Edge(chart, stopping, idle_back, "-", "MOTORS_STOPPED / result=ABORTED", layout=EdgeLayout.RIGHT_RIGHT_CURVED)
+Edge(chart, result_success, idle_back, "-", layout=EdgeLayout.RIGHT_RIGHT_CURVED)
+Edge(chart, result_error, idle_back, "-", layout=EdgeLayout.RIGHT_RIGHT_CURVED)
+Edge(chart, tracking, tracking_back, "-")
+Edge(chart, tracking_back, start_cond_1, "->", "MOTORS_STOPPED / sun_tracker_logic_detect()", layout=EdgeLayout.LEFT_LEFT_CURVED)
 
 # Manually adjust view size because edge text is not yet automatically taken into account
-Point(chart, 4.8, 5)
-Point(chart, -0.5, 0)
+Point(chart, 5.1, 5)
+Point(chart, -1.1, 0)
 
 chart.exportSvg("sun_tracker_state_machine.svg")
